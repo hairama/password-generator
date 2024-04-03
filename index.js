4d7d9d236847e4467bee5fe1dacf470c08e783d1
hairama <67448768+hairama@users.noreply.github.com>
2024-04-02 21:06:45 -0700

    major refactor

--- index.js
+++ index.js
@@ -1,61 +1,83 @@
-const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
-"/"];
+const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+const numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+const symbols = ["~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?","/"]
+
 
 //variables to store & populate the password boxes
-let firstPasswordContainer = document.getElementById("first-password-container")
-let secondPasswordContainer = document.getElementById("second-password-container")
+const firstPasswordContainer = document.getElementById("first-password-container")
+const secondPasswordContainer = document.getElementById("second-password-container")
+const passwordLength = document.getElementById("password-length")
 
-//define passwordGenerate()
-function passwordGenerate() {
+
+//varibles for user-defined parameters
+let pwLength = 15
+let includeNumbers = true
+let includeSymbols = true
+passwordLength.value = pwLength
+
+
+document.addEventListener("click", function(e) {
+    if (e.target.dataset.include_numbers) {
+        includeNumbers = !includeNumbers
+    }
+    if (e.target.dataset.include_symbols) {
+        includeSymbols = !includeSymbols
+    }
+    
+    // click 'generate' button, run generate function
+    if (e.target.dataset.generate) {
+        pwLength = passwordLength.value
+        passwordGenerate(pwLength, includeNumbers, includeSymbols)
+        firstPasswordContainer.style.cursor = "pointer"
+        secondPasswordContainer.style.cursor = "pointer"
+    } 
+    // dark mode button
+    else if (e.target.dataset.dark_mode) {
+        document.body.classList.toggle("dark-mode")
+    } 
+    // copy password fields
+    else if (e.target.dataset.first_pw_box) {
+    // don't copy if blank
+        (firstPasswordContainer.textContent !== '') ? copyToClipboard(e) : undefined;
+    } 
+    else if (e.target.dataset.second_pw_box) {
+        (secondPasswordContainer.textContent !== '') ? copyToClipboard(e) : undefined;
+    }
+})
+
+
+
+function passwordGenerate(pwLength, includeNumbers, includeSymbols) {
+    //build array based on desired length, and whether to include numbers or symbols
+    let specifiedArray = [...characters];
+    
+    if (includeNumbers) {
+        specifiedArray.push(...numbers);
+    }
+    if (includeSymbols) {
+        specifiedArray.push(...symbols);
+    }
+   
     //clear old passwords
     firstPasswordContainer.textContent = ""
     secondPasswordContainer.textContent = ""
 
     //for loop to randomly select characters
-    for (i = 0; i < 15; i++) {
-        /* 1. get a random number between 0 and length of character[] array
-           2. Use this number to get a value from the character[] array
-           3. get text from the 'first-password-container' element and add the value to it */
-        firstPasswordContainer.textContent += characters[Math.floor( Math.random() * characters.length)]
-        //same thing but for 'second-password-container'
-        secondPasswordContainer.textContent += characters[Math.floor( Math.random() * characters.length)]
+    let firstPassword = ''
+    let secondPassword = ''
+    for (i = 0; i < pwLength; i++) {
+        firstPassword += specifiedArray[Math.floor( Math.random() * specifiedArray.length)]
+        secondPassword += specifiedArray[Math.floor( Math.random() * specifiedArray.length)]
     }
+    firstPasswordContainer.textContent = firstPassword
+    secondPasswordContainer.textContent = secondPassword
 }
 
-//define copyToClipboard()
-function copyToClipboard(elementToCopy) {
-    //create a string to hold the text we'll copy to the clipboard
-    let textToCopy = ""
-    
-    //check which text field the user clicked
-    if (elementToCopy === 1) {
-        //grab the text from the relevant html element & write to clipboard
-        textToCopy = firstPasswordContainer.textContent
-        navigator.clipboard.writeText(textToCopy)
-    } else if (elementToCopy === 2) {
-        textToCopy = secondPasswordContainer.textContent
-        navigator.clipboard.writeText(textToCopy)
-    }
-    alert("Copied the text: " + textToCopy)
+
+
+function copyToClipboard(e) {
+    const textContent = e.target.textContent
+    navigator.clipboard.writeText(textContent)
+    alert("Copied the text: " + textContent)
 }
 
-//define darkOrLightMode()
-function darkOrLightMode() {
-    document.body.classList.toggle("dark-mode")
-}
-  
-/*how to populate 2 fields with a single function
-    1. I need to generate a single random password which follows some rules
-    2. after I generate one password I need to store it or update the field
-    3. then I need to generate another password using the same rules
-    and store it or update it somewhere
-    4. I know I need the same code to generate a password, so let's start there
-    Done! 
-    5. Now I could either stick it in a variable (firstPwd) or the <p> directly
-    6. I imagine that having a variable will help with copy-on-click
-    7. Nevermind directly populating the <p> meets the need
-    8. The real question is whether I can copy text from any text element? (<p> <button> etc?) 
-    9. seems like you can.
-    10.So, how do I create a second password?
-    11. Just do it in the same for loop
-*/
