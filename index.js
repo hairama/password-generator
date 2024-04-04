d96ab4fe6f274d69041f81085228eef5dd607f47
hairama <67448768+hairama@users.noreply.github.com>
2024-04-04 16:16:05 -0700

    javascript and HTML cleanup

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const symbols = ["~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","="
 const firstPasswordContainer = document.getElementById("first-password-container")
 const secondPasswordContainer = document.getElementById("second-password-container")
 const passwordLength = document.getElementById("password-length")
+const errorMessage = document.getElementById('password-length-error');
 
 
 //varibles for user-defined parameters
@@ -14,39 +15,65 @@ let pwLength = 15
 let includeNumbers = true
 let includeSymbols = true
 let darkMode = true
+// initializing the input field to default value of 15
 passwordLength.value = pwLength
+errorMessage.textContent = ' '
 
+// event listeners
+document.addEventListener("click", (e) => {
+    handleIncludeNumbersToggle(e)
+    handleIncludeSymbolsToggle(e)
+    handleGenerateButtonClick(e)
+    handleDarkModeToggle(e)
+    handleCopyPassword(e)
+})
 
-document.addEventListener("click", function(e) {
+// event listener functions
+function handleIncludeNumbersToggle(e) {
     if (e.target.dataset.include_numbers) {
         includeNumbers = !includeNumbers
     }
+}
+
+function handleIncludeSymbolsToggle(e) {
     if (e.target.dataset.include_symbols) {
         includeSymbols = !includeSymbols
     }
-    
-    // click 'generate' button, run generate function
+}
+
+function handleGenerateButtonClick(e) {
     if (e.target.dataset.generate) {
-        pwLength = passwordLength.value
-        passwordGenerate(pwLength, includeNumbers, includeSymbols)
-        firstPasswordContainer.style.cursor = "pointer"
-        secondPasswordContainer.style.cursor = "pointer"
-    } 
-    // dark mode button
-    else if (e.target.dataset.dark_mode) {
+        checkInputLength()
+    }
+}
+
+function handleDarkModeToggle(e) {
+    if (e.target.dataset.dark_mode) {
         document.body.classList.toggle("dark-mode")
-    } 
-    // copy password fields
-    else if (e.target.dataset.first_pw_box) {
-    // don't copy if blank
+    }
+}
+
+function handleCopyPassword(e) {
+    if (e.target.dataset.first_pw_box) {
         (firstPasswordContainer.textContent !== '') ? copyToClipboard(e) : undefined;
-    } 
-    else if (e.target.dataset.second_pw_box) {
+    } else if (e.target.dataset.second_pw_box) {
         (secondPasswordContainer.textContent !== '') ? copyToClipboard(e) : undefined;
     }
-})
+}
 
+// other functions
+function checkInputLength() {
+    const pwLength = parseInt(passwordLength.value)
 
+    if (isNaN(pwLength) || pwLength.length > 3 || pwLength > 128 || pwLength <= 0) {
+        errorMessage.textContent = "Please enter a number between 1 and 128.";
+        passwordLength.setCustomValidity("Invalid input"); // Set a custom validity message
+    } else {
+        errorMessage.textContent = " ";
+        passwordLength.setCustomValidity(""); // Reset validity message
+        passwordGenerate(pwLength, includeNumbers, includeSymbols)
+    }
+}
 
 function passwordGenerate(pwLength, includeNumbers, includeSymbols) {
     //build array based on desired length, and whether to include numbers or symbols
@@ -70,11 +97,14 @@ function passwordGenerate(pwLength, includeNumbers, includeSymbols) {
         firstPassword += specifiedArray[Math.floor( Math.random() * specifiedArray.length)]
         secondPassword += specifiedArray[Math.floor( Math.random() * specifiedArray.length)]
     }
+    //put passwords in pw-box elements
     firstPasswordContainer.textContent = firstPassword
     secondPasswordContainer.textContent = secondPassword
+
+    //add the pointer style, now that the pw-boxes aren't empty
+    firstPasswordContainer.style.cursor = "pointer"
+    secondPasswordContainer.style.cursor = "pointer"
 }
-
-
 
 function copyToClipboard(e) {
     const textContent = e.target.textContent
@@ -82,6 +112,7 @@ function copyToClipboard(e) {
     alert("Copied the text: " + textContent)
 }
 
+//ensure that include numbers, include symbols and Dark mode are all "on" by default
 function setInitialToggleState() {
     document.getElementById('toggle-symbols').checked = includeSymbols
     document.getElementById('toggle-numbers').checked = includeNumbers
